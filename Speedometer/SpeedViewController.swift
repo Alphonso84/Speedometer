83c3d54e1825bf74f9c0c776386d5f6430307fb0
Alphonso Bret Sensley II <fonzman1984@me.com>
2018-05-15 00:34:32 -0700

    Building out UI for simple Speedometer App

---
+++ Speedometer/SpeedViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SpeedViewController.swift
+//  Speedometer
+//
+//  Created by user on 5/15/18.
+//  Copyright © 2018 Alphonso Sensley II. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SpeedView: UIViewController {
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    override func viewDidLoad() {
+        backgroundImage.image = #imageLiteral(resourceName: "Background")
+    }
+ 
+   
+    
+}
